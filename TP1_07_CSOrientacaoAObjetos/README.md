e2ad28cfc5c78dca282abad878f0ec1327be2c8e
marcuslaraa <marcus.lara@hotmail.com>
2024-11-02 00:26:40 -0300

    add readme

---
+++ TP1_07_CSOrientacaoAObjetos/README.md
@@ -0,0 +1,14 @@
+## Descrição do Projeto
+
+Desenvolva uma aplicação que contenha diversos alunos (Cada aluno tem número de RA, nome e um Array com 4 notas) armazenados em uma lista.
+
+### Funcionalidades
+
+1. **Cadastrar Aluno**: Leia todos os dados do aluno.
+2. **Exibir Situações**: Exiba os alunos aprovados, de recuperação e reprovados.
+
+### Critérios de Avaliação
+
+- **Aprovados**: Média >= 6
+- **Recuperação**: Média >= 4 e Média < 6
+- **Reprovados**: Média < 4
--- TP1_07_CSOrientacaoAObjetos/obj/Debug/net8.0/TP1_07_CSOrientacaoAObjetos.AssemblyInfo.cs
+++ TP1_07_CSOrientacaoAObjetos/obj/Debug/net8.0/TP1_07_CSOrientacaoAObjetos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP1_07_CSOrientacaoAObjetos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b136604f805e46c5e814a7a69f2b0a30f519e14")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP1_07_CSOrientacaoAObjetos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP1_07_CSOrientacaoAObjetos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
